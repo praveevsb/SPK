d6cb614147dd984324c26e790a8d3d392c415f2b
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-11 11:05:28 +0000

    Update large3.c

--- large3.c
+++ large3.c
@@ -3,10 +3,10 @@ void main()
 {
 int a,b,c;
 printf("enter three numbers");
-scanf("%d%d%d",a,b,c);
+scanf("%d%d%d",&a,&b,&c);
 if(a>=b&&a>=c)
-printf("5d is large",a);
-elseif(b>=a&&b>=c)
+printf("%d is large",a);
+else if(b>=a&&b>=c)
 printf("%d is large",b);
 else
 printf("%d is a large",c);
