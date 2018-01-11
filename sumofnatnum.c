9636ae8b54dca87ed2b38ad856c0d9569713e201
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-11 11:26:35 +0000

    Update sumofnatnum.c

--- sumofnatnum.c
+++ sumofnatnum.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-int i, n,sum;
+int i,n,sum=0;
 printf("enter a number\n");
 scanf("%d",n);
 for(i=0;i<=n;i++)
 {
-sum=sum + i;
+sum=sum+i;
 }
 printf("sum of first %d natural number =%d\n",n,sum);
 }
