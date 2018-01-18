cd6a9af12dc77c28a7c5c829a919b3b1d30532a5
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-18 11:12:30 +0000

    Create sumint.c

---
+++ sumint.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n,k,i,s=0,a[n];
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<k;i++)
+{
+s=s+a[i];
+}
+printf("sum of first %d num is %d",k,s;
+}
