9dbdc00366c54c782474af9b7d758d89d9307ef3
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-19 16:40:08 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -3,17 +3,21 @@ int main()
 {
 int num,rem,sum,s=0;
 printf("enter the number:");
-scanf("%d",num);
+scanf("%d",&num);
 while(num>0)
 {
 rem=num%10;
-sum=rem*rem*rem:
+sum=rem*rem*rem;
 s=sum+s;
 num=num/10;
 }
 if(num==s)
+{
 printf("yes");
+}
 else
-print("no");
+{
+printf("no");
+}
 return 0;
 }
