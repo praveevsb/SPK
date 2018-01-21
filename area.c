41cc3c5bba47ecf177fa9fc34f66d221d6620f22
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-21 14:50:48 +0530

    Create area.c

---
+++ area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float len,bre,area;
+printf("enter the length and breadth::");
+scanf("%f%f",&len,&bre);
+area=len*bre;
+printf("area is %.5f",area);
+return 0;
+}
