b40963e305668e0f19c8b129edc6921e34f4542b
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-21 13:54:04 +0530

    Create strcap.c

---
+++ strcap.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a[199];
+int i,len;
+len=strlen(a);
+for(i=0;i<=j;i++)
+{
+if(i==0)
+{
+a[i]=toupper(a[i]);
+}
+if(i==' ')
+{
+a[i=1]=toupper(a[i+1]);
+}
+}
+printf("%s",a);
+getch();
+return o;
+}
