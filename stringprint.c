34238b177c051bb371d1ceaec61681ede7f73e34
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-21 15:07:28 +0530

    Create stringprint.c

---
+++ stringprint.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+char a[50];
+printf("entr the string:");
+scanf("%s",&a);
+printf("enter the value:");
+scanf("%d",&j);
+for(i=0;i<=j;i++)
+{
+printf("%s",a);
+}
+return 0;
+}
